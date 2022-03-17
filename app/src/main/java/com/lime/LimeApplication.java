3ded6ffbda72fab909a65948cd2075101cf86aaa
banana1019 <jyho930@gmail.com>
2022-03-17 16:35:04 +0900

    정윤

--- app/src/main/java/com/lime/LimeApplication.java
+++ app/src/main/java/com/lime/LimeApplication.java
@@ -5,7 +5,9 @@ package com.lime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class LimeApplication {
 
