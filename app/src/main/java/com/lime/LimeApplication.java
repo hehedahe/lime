7c9fa37d918b9d8f40148efbddf56223ce1fcf6e
poohbearr <syb369@naver.com>
2022-03-23 23:41:03 +0900

    .

--- app/src/main/java/com/lime/LimeApplication.java
+++ app/src/main/java/com/lime/LimeApplication.java
@@ -2,24 +2,17 @@ package com.lime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-=======
-
->>>>>>> main
 @SpringBootApplication
 public class LimeApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LimeApplication.class, args);
   }
-<<<<<<< HEAD
 
   public String getGreeting() {
     return "Hello World!";
   }
-=======
->>>>>>> main
 }
