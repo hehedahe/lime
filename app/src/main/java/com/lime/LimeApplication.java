c331bbfdfb7f8342d87ce262570dc4e69f8f46ee
hehedahe <smyekgo@gmail.com>
2022-03-24 01:32:23 +0900

    header css 수정

--- app/src/main/java/com/lime/LimeApplication.java
+++ app/src/main/java/com/lime/LimeApplication.java
@@ -1,43 +1,14 @@
 package com.lime;
 
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @SpringBootApplication
 public class LimeApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(LimeApplication.class, args);
-  }
-
-  //@Bean
-  public CommandLineRunner commandLineRunner(ApplicationContext beanContainer) {
-    return args -> {
-
-      System.out.println("빈 컨테이너가 생성한 객체(빈 컨테이너에 들어 있는 객체):");
-
-      String[] beanNames = beanContainer.getBeanDefinitionNames();
-      for (int i = 0; i < beanNames.length; i++) {
-        Object bean = beanContainer.getBean(beanNames[i]);
-        System.out.printf("--->%03d: %s\n", i + 1, bean.getClass().getName());
-      }
-
-    };
-  }
-
-  @RequestMapping("/hello")
-  String hello() {
-    return "Hello World!";
-  }
-
-  public String getGreeting() {
-    return "Hello World!";
   }
 }
 
