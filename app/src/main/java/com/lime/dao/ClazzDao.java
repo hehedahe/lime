06fd0cb4e15d698d47b2d0f27385ba86aea392ac
Polarvear <znzpzp13@naver.com>
2022-04-01 18:26:25 +0900

    change ClazzDao.java

--- app/src/main/java/com/lime/dao/ClazzDao.java
+++ app/src/main/java/com/lime/dao/ClazzDao.java
@@ -2,8 +2,11 @@ package com.lime.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
 import com.lime.domain.Clazz;
 
+@Mapper
 public interface ClazzDao {
 	List<Clazz> findAll();
 }
