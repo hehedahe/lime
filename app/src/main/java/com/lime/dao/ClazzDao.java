d50611a1bad43848f694e15be34ff876ae096427
hehedahe <smyekgo@gmail.com>
2022-04-02 18:35:30 +0900

    resolved conflict

--- app/src/main/java/com/lime/dao/ClazzDao.java
+++ app/src/main/java/com/lime/dao/ClazzDao.java
@@ -1,16 +1,10 @@
 package com.lime.dao;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
 import com.lime.domain.Clazz;
 
-<<<<<<< HEAD
-
-=======
->>>>>>> Polarvear
 @Mapper
 public interface ClazzDao {
-	List<Clazz> findAll();
+  List<Clazz> findAll();
 }
