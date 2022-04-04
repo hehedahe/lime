b702a0feb3a56bd9586b5198008f3ba91be11fb4
hehedahe <smyekgo@gmail.com>
2022-04-04 23:21:43 +0900

    field 테이블에 latlng 컬럼 추가

--- app/src/main/java/com/lime/controller/ClazzController.java
+++ app/src/main/java/com/lime/controller/ClazzController.java
@@ -3,21 +3,20 @@ package com.lime.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.lime.service.ClazzService;
 
 @RestController
 public class ClazzController {
 
-	@Autowired
-	ClazzService clazzService;
-	
-	@RequestMapping("/class/list")
-	public Object classList() {
-		return clazzService.clazzList();
-	}
-	
-	@RequestMapping("/class/add") {
-		public Object 
-	}
+  @Autowired
+  ClazzService clazzService;
+
+  @RequestMapping("/class/list")
+  public Object classList() {
+    return clazzService.clazzList();
+  }
+
+  //	@RequestMapping("/class/add") {
+  //		public Object 
+  //	}
 }
