4892d2b9ced61af988848133381077bcea282a8d
banana1019 <jyho930@gmail.com>
2022-04-23 22:04:31 +0900

    결제페이지 구현 중

--- app/src/main/java/com/lime/controller/AutoLoginListener.java
+++ app/src/main/java/com/lime/controller/AutoLoginListener.java
@@ -9,20 +9,18 @@ import com.lime.domain.Member;
 //역할:
 //- 요청이 들어 올 때, 기능 테스트를 위해 자동으로 로그인 시킨다.
 //
-
 //@WebListener
-/*
 public class AutoLoginListener implements ServletRequestListener {
   @Override
   public void requestInitialized(ServletRequestEvent sre) {
     System.out.println("AutoLoginListener.requestInitialized() 호출됨!");
     Member loginUser = new Member();
-    loginUser.setNo(2);
-    loginUser.setName("user2");
+    loginUser.setNo(227);
+    loginUser.setName("user227");
 
     HttpServletRequest httpRequest = (HttpServletRequest) sre.getServletRequest(); 
     HttpSession session = httpRequest.getSession();
     session.setAttribute("loginUser", loginUser);
   }
 }
- */
+
