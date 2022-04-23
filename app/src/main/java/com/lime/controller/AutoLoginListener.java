e706069f915d907e9183680b8fd8a0cc2646a54a
banana1019 <jyho930@gmail.com>
2022-04-24 03:51:13 +0900

    .

--- app/src/main/java/com/lime/controller/AutoLoginListener.java
+++ app/src/main/java/com/lime/controller/AutoLoginListener.java
@@ -2,7 +2,6 @@ package com.lime.controller;
 
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
-import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import com.lime.domain.Member;
@@ -10,7 +9,7 @@ import com.lime.domain.Member;
 //역할:
 //- 요청이 들어 올 때, 기능 테스트를 위해 자동으로 로그인 시킨다.
 //
-@WebListener
+//@WebListener
 public class AutoLoginListener implements ServletRequestListener {
   @Override
   public void requestInitialized(ServletRequestEvent sre) {
