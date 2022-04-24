66ce497aec2cc5551ee6918734db50b81e530342
omh1004 <ohchef2020@gmail.com>
2022-04-24 19:20:32 +0900

    '로그인처리전페이지적용'

--- app/src/main/java/com/lime/controller/AutoLoginListener.java
+++ app/src/main/java/com/lime/controller/AutoLoginListener.java
@@ -10,7 +10,7 @@ import com.lime.domain.Member;
 //역할:
 //- 요청이 들어 올 때, 기능 테스트를 위해 자동으로 로그인 시킨다.
 //
-@WebListener
+//@WebListener
 public class AutoLoginListener implements ServletRequestListener {
   @Override
   public void requestInitialized(ServletRequestEvent sre) {
