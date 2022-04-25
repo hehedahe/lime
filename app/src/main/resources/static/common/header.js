51e20e7772088af927e90a1026ad48c29c6f6629
Polarvear <znzpzp13@naver.com>
2022-04-26 02:27:58 +0900

    로그인 헤더 구현

--- app/src/main/resources/static/common/header.js
+++ app/src/main/resources/static/common/header.js
@@ -7,7 +7,8 @@ fetch('/member/getLoginUser').then(function (response) {
     if (result.status == "success") {
         $('#u-nav-links').html(`
             <div id="u-links-cal">
-                <a href="/social-match/rsv.html">
+                <a href="/social-match/rsv.html"><link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+                <link rel="stylesheet" href="/header/headertest-1.css" type="text/css">
                     <img src="/asset/image/header/calendar.png" height="30px" width="30px">
                 </a>
             </div>
@@ -15,14 +16,13 @@ fetch('/member/getLoginUser').then(function (response) {
                 <img src="/asset/image/header/msg2.png" height="30px" width="30px">
             </div>
             <div class="btn-group dropend">
-                <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><img src="../header/userImg.png" height="30px" width="30px"/>
-                     Dropend
+                <button id="prof-img" type="button" class="btn" data-bs-toggle="dropdown" aria-expanded="false"><img src="../header/userImg.png" height="30px" width="30px"/>
                 </button>
                 <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="#">마이페이지</a></li>
-                    <li><a class="dropdown-item" href="#">나의활동</a></li>
+                    <li><a class="dropdown-item" href="#">나의 활동</a></li>
                     <li><a class="dropdown-item" href="#">찜목록</a></li>
-                    <li><a class="dropdown-item" href="#">로그아웃</a></li>
+                    <li><a class="dropdown-item" href="#"style="color: red;" id="logoutBtn">로그아웃</a></li>
                 </ul>
             </div>
         `)
@@ -32,6 +32,7 @@ fetch('/member/getLoginUser').then(function (response) {
                 <li><a href="/login/login.html">로그인</a></li>
                 <li><a href="#">|</a></li>
                 <li><a href="/signup/signup1.html">회원가입</a></li>`)
+                location.href = "/social-match/index.html"
             });
         });
     }
@@ -39,8 +40,3 @@ fetch('/member/getLoginUser').then(function (response) {
 
 
 
-.btn-secondary {
-    /* color: #fff; */
-    /* background-color: #6c757d; */
-    /* border-color: #6c757d; */
-}
