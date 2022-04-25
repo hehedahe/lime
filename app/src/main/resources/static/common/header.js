42e840894265171a1982dedd3bbd2ab03229b686
Polarvear <znzpzp13@naver.com>
2022-04-25 15:22:36 +0900

    컨플릭 해결

--- app/src/main/resources/static/common/header.js
+++ app/src/main/resources/static/common/header.js
@@ -14,17 +14,6 @@ fetch('/member/getLoginUser').then(function (response) {
             <div id="u-links-msg">
                 <img src="/asset/image/header/msg2.png" height="30px" width="30px">
             </div>
-<<<<<<< HEAD
-            <div id="u-links-user">
-<!--            <button id=logoutBtn onclick="test()">버튼</button>-->
-<!--                <div id="myDropdown" class="dropdown-content">-->
-<!--                    <a href="#">Link 1</a>-->
-<!--                    <a href="#">Link 2</a>-->
-<!--                    <a href="#">Link 3</a>-->
-<!--                </div> -->
-<!--                <img src="../asset/image/user2b.png" height="30px" width="30px">-->
-            </div>
-=======
             <ul class="nav nav-pills id="u-links-user"">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><img src="../header/userImg.png" height="30px" width="30px"/></a>
@@ -36,7 +25,6 @@ fetch('/member/getLoginUser').then(function (response) {
                         </ul>
                 </li>
             </ul>
->>>>>>> il
         `)
         $('#logoutBtn').on('click', function() {
             fetch("/member/signout").then(function(response) {
@@ -48,42 +36,4 @@ fetch('/member/getLoginUser').then(function (response) {
         });
     }
 });
-<<<<<<< HEAD
 
-
-window.addEventListener('load', function() {
-    // alert("okokok")
-    const logoutBtn = this.document.querySelector(".logoutBtn")
-    console.log(logoutBtn)
-})
-
-//$(document).on('click', )
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    alert("okok2")
-
-
-    const logoutBtn = document.querySelector(".logoutBtn")
-    console.log(logoutBtn)
-    
- }, false);
-
-
-// window.onload = function() {
-//     alert(:)
-// }
-
-
- function test() {
-     const h1Tag = document.createElement("h1")
-     const hihi = document.createTextNode('Hi!')
-
-     h1Tag.appendChild(hihi)
-
-     document.this.appendChild(h1Tag)
- }
-
-
-=======
->>>>>>> il
