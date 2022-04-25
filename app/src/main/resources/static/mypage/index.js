7b2631c77fcfaf8854f810c618dec30a9bc8dc9b
banana1019 <jyho930@gmail.com>
2022-04-25 15:17:30 +0900

    마이페이지 유저 정보 불러오기

--- app/src/main/resources/static/mypage/index.js
+++ app/src/main/resources/static/mypage/index.js
@@ -1,1 +1,42 @@
 "use strict";
+
+$.getJSON("/member/getLoginUser", (result) => {
+    console.log(result.status);
+    if (result.status == "fail") {
+      location.href = `/login/login.html`
+    }
+    // console.log(result.status);
+  })
+
+const xName = $("#user-name");
+const xRegionHand = $("#user-region-hand");
+const xClub = $("#user-club");
+const xBalance = $("#balance");
+const xLevel = $("#user-level");
+const xManner = $("#user-manner");
+
+$.getJSON("/rsv/match/balance", function (result) {
+    console.log(result);
+    let userInfo = result.data;
+    xName.text(userInfo.name)
+    xRegionHand.text(`${userInfo.region} | ${userInfo.hand}`);
+    xClub.text(userInfo.club)
+    xBalance.text(`${(userInfo.sum).toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",")}원`)
+    xLevel.text(`${checkLevel(userInfo.level)}`)
+    xManner.text(`${userInfo.mannerScore} Point`)
+  })
+
+  function checkLevel(courtTypeNo) {
+    switch (courtTypeNo) {
+        case 'D':
+            return "DEVELOPMENT";
+        case 'B':
+            return "BEGINNER";
+        case 'I':
+            return "INTERMEDIATE";
+        case 'A':
+            return "ADVANCED";
+        case 'P':
+            return "PRO";
+    }
+}
