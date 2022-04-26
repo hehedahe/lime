6a586bafc754311d7fa78522c7669729438eada3
poohbearr <syb369@naver.com>
2022-04-26 10:29:40 +0900

    ..

--- app/src/main/java/com/lime/controller/MarketController.java
+++ app/src/main/java/com/lime/controller/MarketController.java
@@ -111,6 +111,34 @@ public class MarketController {
     }
   }
 
+  @RequestMapping("/market/update")
+  public Object update(Market market, HttpSession session) {
+    Member member = (Member) session.getAttribute("loginUser");
+    market.setUserId(member.getNo());
+    System.out.println(market);
+    //int count = marketService.update(market);
+    return 1;
+    //    if (count == 1) {
+    //      return new ResultMap().setStatus(SUCCESS);
+    //    } else {
+    //      return new ResultMap().setStatus(FAIL).setData("게시글 번호가 유효하지 않거나 게시글 작성자가 아닙니다.");
+    //    }
+  }
+
+  @RequestMapping("/market/updateState")
+  public Object updateState(Market market, HttpSession session) {
+    Member member = (Member) session.getAttribute("loginUser");
+    market.setUserId(member.getNo());
+    System.out.println(market);
+    int count = marketService.updateState(market);
+
+    if (count == 1) {
+      return new ResultMap().setStatus(SUCCESS);
+    } else {
+      return new ResultMap().setStatus(FAIL).setData("게시글 번호가 유효하지 않거나 게시글 작성자가 아닙니다.");
+    }
+  }
+
   @RequestMapping("/market/like")
   public Object update(ItemLike itemLike, HttpSession session) {
     Member member = (Member) session.getAttribute("loginUser");
@@ -129,7 +157,6 @@ public class MarketController {
   @RequestMapping("/market/getLike")
   public Object getLike(HttpSession session) {
     Member member = (Member) session.getAttribute("loginUser");
-    ItemLike itemLike = new ItemLike();
     System.out.println("member::::::::" + member);
 
     if (member != null) {
