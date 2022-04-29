12c8bb0e99d8b80891a0f3fc5c05a89051a06656
Polarvear <znzpzp13@naver.com>
2022-04-29 13:46:21 +0900

    adding email

---
+++ app/src/main/java/com/lime/service/impl/DefaultMailSender.java
@@ -0,0 +1,18 @@
+package com.lime.service.impl;
+
+import javax.servlet.http.HttpSession;
+import com.lime.service.MailService;
+
+public class DefaultMailSender implements MailService{
+
+  @Override
+  public void mailSend(HttpSession session, String email) {
+
+    //    try {
+    //      Mailhandler mailHandler = new MailHandler(JavaMailSenderImpl);
+    //
+    //    }
+    //  }
+
+  }
+}
