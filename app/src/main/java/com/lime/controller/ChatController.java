e966c4e33e9eea4bb639cf797ea8ea65ac4acd70
omh1004 <ohchef2020@gmail.com>
2022-05-01 23:20:00 +0900

    '채팅수정'

--- app/src/main/java/com/lime/controller/ChatController.java
+++ app/src/main/java/com/lime/controller/ChatController.java
@@ -1,7 +1,7 @@
 package com.lime.controller;
 
 import java.time.LocalDateTime;
-import org.jgroups.demos.Chat;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -18,27 +19,27 @@ import reactor.core.scheduler.Schedulers;
 @RestController // 데이터 리턴 서버
 public class ChatController {
 
-  private final ChatRepository chatRepository;
-
-  // 귓속말 할때 사용하면 되요!!
-  @CrossOrigin
-  @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
-    return chatRepository.mFindBySender(sender, receiver)
-        .subscribeOn(Schedulers.boundedElastic());
-  }
-
-  @CrossOrigin
-  @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-  public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
-    return chatRepository.mFindByRoomNum(roomNum)
-        .subscribeOn(Schedulers.boundedElastic());
-  }
-
-  @CrossOrigin
-  @PostMapping("/chat")
-  public Mono<Chat> setMsg(@RequestBody Chat chat){
-//    chat.setCreatedAt(LocalDateTime.now());
-    return chatRepository.save(chat); // Object를 리턴하면 자동으로 JSON 변환 (MessageConverter)
-  }
+    private final ChatRepository chatRepository;
+    
+    // 귓속말 할때 사용하면 되요!!
+    @CrossOrigin
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
+        return chatRepository.mFindBySender(sender, receiver).subscribeOn(Schedulers.boundedElastic());
+    }
+    
+    @CrossOrigin
+    @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+        return chatRepository.mFindByRoomNum(roomNum)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+    
+    @CrossOrigin
+    @PostMapping("/chat") 
+    public Mono<Chat> setMsg(@RequestBody Chat chat){
+        System.out.println(chat);
+         chat.setCreatedAt(LocalDateTime.now());
+        return chatRepository.save(chat); // Object를 리턴하면 자동으로 JSON 변환 (MessageConverter)
+    }
 }
