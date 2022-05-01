78e757e6a30ba8ecf4561a9b7ebf955e47f8716e
omh1004 <ohchef2020@gmail.com>
2022-05-01 19:35:54 +0900

    '컨플릭트확인'

--- app/src/main/java/com/lime/controller/ChatController.java
+++ app/src/main/java/com/lime/controller/ChatController.java
@@ -39,6 +39,6 @@ public class ChatController {
   @PostMapping("/chat")
   public Mono<Chat> setMsg(@RequestBody Chat chat){
 //    chat.setCreatedAt(LocalDateTime.now());
-    return chatRepository.save(chat); // Object를 리턴하면 자동으로 JSON 변환 (MessageConverter)
+    return chatRepository.save(chat); // Object를 리턴하면 자동으로 JSON 변환 (MessageConverter)   
   }
 }
